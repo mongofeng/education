8b1f34a51f6d906e8351d8c20741c8b25f34a1d1
yangpu <18312701884@163.com>
2019-04-05 15:42:30 +0800

    feat:添加别名

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,10 @@
 
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+// https://github.com/arackaf/customize-cra
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra');
+const path = require('path');
+function resolve(dir) {
+    return path.join(__dirname, '.', dir)
+}
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
@@ -10,4 +15,7 @@ module.exports = override(
    javascriptEnabled: true,
    modifyVars: { '@primary-color': 'red' },
  }),
+ addWebpackAlias({
+  '@': resolve('src')
+ })
 );
