d6cbf774c26bd9e2312f04cb55074bb5d15c644f
yangpu <18312701884@163.com>
2019-04-24 22:05:46 +0800

    feat:添加antd-design-pro

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,30 @@
-
 // https://github.com/arackaf/customize-cra
-const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra');
-const path = require('path');
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addWebpackAlias
+} = require("customize-cra");
+const path = require("path");
 function resolve(dir) {
-    return path.join(__dirname, '.', dir)
+  return path.join(__dirname, ".", dir);
 }
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-   style: true,
+  fixBabelImports("antd", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
   }),
- addLessLoader({
-   javascriptEnabled: true,
-   modifyVars: { '@primary-color': 'red' },
- }),
- addWebpackAlias({
-  '@': resolve('src')
- })
+  // fixBabelImports("ant-design-pro", {
+  //   libraryName: "ant-design-pro",
+  //   libraryDirectory: "lib",
+  //   style: true,
+  // }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { "@primary-color": "red" }
+  }),
+  addWebpackAlias({
+    "@": resolve("src")
+  })
 );
