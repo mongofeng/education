b21b22ef4819f6c448551e6fed66cb84485f6263
yangpu <yangpu@xlink.cn>
2019-08-02 12:24:40 +0800

    build(source maps): set to false, source maps are not generated for a production build. This solves

--- config-overrides.js
+++ config-overrides.js
@@ -9,13 +9,8 @@ const path = require("path");
 function resolve(dir) {
   return path.join(__dirname, ".", dir);
 }
-const rewiredMap = () => config => {
-  config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
-  return config;
-};
+
 module.exports = override(
-  // 关闭mapSource
-  rewiredMap(),
   fixBabelImports("antd", {
     libraryName: "antd",
     libraryDirectory: "es",
