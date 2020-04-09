b3ac2ec37f2e5501c578bef3ebc6b54977ffbba6
yangpu <yangpu@xlink.cn>
2020-04-09 16:42:26 +0800

    feat: 修改地址

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,18 @@
 /* 2.然后创建 src/setupProxy.js 并写入一下转发规则 */
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
-    app.use(createProxyMiddleware('/v1', {
+    app.use(createProxyMiddleware('/v2', {
         target: 'http://127.0.0.1:8110/',
         pathRewrite: {
-            "^/v1": "/"
+            "^/v2": "/"
         }
     }));
 
 
-    app.use(createProxyMiddleware('/wechatServer', {
-        target: 'http://127.0.0.1:8110/',
-        pathRewrite: {
-            "^/wechatServer": "/wechat"
-        }
-    }));
+    // app.use(createProxyMiddleware('/wechatServer', {
+    //     target: 'http://127.0.0.1:8110/',
+    //     pathRewrite: {
+    //         "^/wechatServer": "/wechat"
+    //     }
+    // }));
 };
