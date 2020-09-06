dfdeb27e39c8ea0711dc28669017ab9e53eb13fd
yangpu <mongofeng@163.com>
2020-09-06 18:11:58 +0800

    feat: 去掉删除的入口

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import { COURSE_HOUR_ACTION_TYPE } from '../const/enum'
 
 // export const isDev: boolean = process.env.NODE_ENV === 'development';
 
-export const isDev: boolean = true;
+export const isDev: boolean = false;
 export const sign:string = 'yMWwcsc25qNY-24GYSD_KTvVbFIQFPLZuzzP_YyXTgM' // 签到提醒
 export const packageBuy:string = 'qcf3mZG_XkEwkVYuV4IKk53H44tcyU70kW9xsnn24lM' // 添加课程包
 export const supplement:string = 'AQnWg1pIJRJvhD4p1HmCLluQn0EFHwLAGHDubmyWN1g' // 补签
