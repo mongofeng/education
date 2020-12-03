da5f44803be6b05c56bb9533051d36ef12d3193c
yangpu <yangpu@xiao100.com>
2020-12-03 10:52:54 +0800

    fix: 修复筛选的问题

--- src/page/student-hour/List.tsx
+++ src/page/student-hour/List.tsx
@@ -7,7 +7,7 @@ import { downLoad } from '@/utils/excel';
 import { PaginationProps } from 'antd/lib/pagination'
 import { commonQuery } from '../../api/statistics'
 import { connect } from 'react-redux'
-
+import * as enums from '../../const/enum'
 
 const Search = Input.Search;
 
@@ -19,29 +19,12 @@ interface IProps {
 
 function List(props: IProps): JSX.Element {
 
-  const columns: Array<ColumnProps<IStudent>> = [
+  const columns: Array<ColumnProps<any>> = [
     {
       title: "姓名",
       dataIndex: "name",
       key: "name",
     },
-    {
-      title: '总课时',
-      dataIndex: 'count',
-    },
-    {
-      title: '金额',
-      dataIndex: 'amount',
-    },
-    {
-      title: "激活课时",
-      dataIndex: "activiteCount",
-    },
-    {
-      title: "未激活课时",
-      dataIndex: "unActiviteCount"
-    },
-
     {
       title: "剩余课时",
       dataIndex: "surplus",
@@ -53,11 +36,35 @@ function List(props: IProps): JSX.Element {
       dataIndex: "used",
 
     },
+    {
+      title: '总课时',
+      dataIndex: 'count',
+    },
+ 
+    {
+      title: "激活课时",
+      dataIndex: "activiteCount",
+    },
+    {
+      title: "未激活课时",
+      dataIndex: "unActiviteCount"
+    },
+
+    
 
     {
       title: "已过期",
       dataIndex: "overdueCount",
 
+    },
+    {
+      title: '金额',
+      dataIndex: 'amount',
+    },
+    {
+      title: "状态",
+      dataIndex: "status",
+      render: (str: enums.STUDENT_STATUS, data: {student: IStudent}) => <span>{enums.STUDENT_STATUS_LABEL[data.student.status]}</span>
     },
   ];
 
@@ -85,10 +92,19 @@ function List(props: IProps): JSX.Element {
       "{$group:{_id:'$studentIds', n: {$sum: 1 },amount:{$sum:'$amount'},count:{$sum:'$count'},used:{$sum:'$used'},surplus:{$sum:'$surplus'},overdueCount:{$sum:{$cond:['$beOverdue','$surplus',0]}},activiteCount:{$sum:{$cond:['$isActive','$count',0]}},unActiviteCount:{$sum:{$cond:['$isActive',0,'$count']}}}}",
       "{$lookup:{from:'student',let:{stuId:{$toObjectId:'$_id'}},pipeline:[{$match:{$expr:{$eq:['$_id','$$stuId']}}}],as:'student'}}",
       "{$project:{id:{$toString: '$_id'},student:{$arrayElemAt:['$student',0]},amount:1,count:1,used:1,surplus:1,overdueCount:1,activiteCount:1,unActiviteCount:1, n:1}}",
+      JSON.stringify({
+        $match : { 'student.status' : 1 }
+      }),
       "{ $sort : { surplus : 1, count: -1 } }",
       
     ]
 
+    if (keyword) {
+      const match = { $match : { 'student.name' : keyword } }
+      sql.push(JSON.stringify(match))
+    }
+
+    // 分页
     if (pager) {
       const skip = (pager.current - 1) * pager.pageSize
       const limit = pager.pageSize
@@ -96,10 +112,7 @@ function List(props: IProps): JSX.Element {
       sql.push(JSON.stringify({$limit : limit }))
     }
 
-    if (keyword) {
-      const match = { $match : { 'student.name' : keyword } }
-      sql.push(JSON.stringify(match))
-    }
+    
 
     return {
       collectionName: "student-package",
@@ -128,7 +141,7 @@ function List(props: IProps): JSX.Element {
 
 
   const onSearch = (key: string) => {
-    setParams(params({pager: pagination, keyword: key}))
+    setParams(params({pager: initPagination, keyword: key}))
   }; 
 
 
