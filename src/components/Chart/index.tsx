3970ac3927f8d180cdf0f3b842d9b5afa0b41287
yangpu <yangpu@xiao100.com>
2021-01-20 17:05:39 +0800

    fix: 修复图表的重复渲染

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,58 +1,54 @@
 import * as React from 'react';
 import echarts from 'echarts'
 import { debounce } from '../../utils/util';
-const { useState, useEffect, useRef } = React;
+const { useEffect, useRef } = React;
 
 interface IProps {
-  option: any
-  height: string
+  option: any;
+  height: string;
 }
 
-const Chart =  function (props) {
+const Chart =  function (props: IProps) {
 
   const containerRef = useRef(null);
+  const echartsInstance = useRef<any>(null)
 
-  const [echartsInstance, setechartsInstance] = useState(null)
-
-  useEffect(() => {
-    console.log('chart重新渲染')
-    if (echartsInstance && props.option) {
-      echartsInstance.setOption(props.option); 
-    }
-    
-    return () => {
-
-    }
-  }, [props.option])
 
 
   useEffect(() => {
+    console.log('初始化渲染图表')
     // 基于准备好的dom，初始化echarts实例
     const myChart = echarts.init(containerRef.current);
-    // // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(props.option);
-
-
-    setechartsInstance(myChart)
+    echartsInstance.current = myChart
 
     const func = debounce(() => {
       console.log('resize')
       myChart.resize()
     }, 300)
 
-    
 
     window.addEventListener('resize', func, false)
     return () => {
-      console.log(echartsInstance)
+      console.log('图标卸载，取消时间')
       window.removeEventListener('resize', func, false)
       myChart.dispose()
-
     }
   }, [])
+
+
+  
+  useEffect(() => {
+    console.log('第二个钩子渲染')
+    if (echartsInstance.current && props.option) {
+      echartsInstance.current.setOption(props.option); 
+    } 
+  }, [props.option])
+
+
+  
   return (
     <div ref={containerRef} style={{width: '100%', height: props.height || '100%'}}></div>
   )
 }
 
-export default React.memo(Chart)
+export default React.memo(Chart, (prev, next) => JSON.stringify(prev) === JSON.stringify(next))
