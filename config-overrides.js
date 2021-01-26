105a5486300c0cf9b319831c4c365f5eb0746208
yangpu <yangpu@xiao100.com>
2021-01-26 16:41:46 +0800

    feat: 添加微前端的样式

--- config-overrides.js
+++ config-overrides.js
@@ -6,6 +6,7 @@ const {
   fixBabelImports,
   addLessLoader,
   addWebpackAlias,
+  overrideDevServer,
   addWebpackPlugin
 } = require("customize-cra");
 const path = require("path");
@@ -13,29 +14,71 @@ function resolve(dir) {
   return path.join(__dirname, ".", dir);
 }
 
-module.exports = override(
-  fixBabelImports("antd", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: true
-  }),
-  addWebpackPlugin(
-    new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
-      threshold: 10240,
-      minRatio: 0.8,
-      cache: true
-    }),
-  ),
 
-  addLessLoader({
-    javascriptEnabled: true,
-    // modifyVars: { "@primary-color": "red" }
-  }),
-  addWebpackAlias({
-    "@": resolve("src"),
-    "src": resolve("src")
-  })
-);
+const { name } = require('./package');
+
+
+const adjustOutput = () => config => {
+  config.output.library = `${name}-[name]`;
+  config.output.libraryTarget = 'umd';
+  config.output.jsonpFunction = `webpackJsonp_${name}`;
+  config.output.globalObject = 'window';
+
+  console.log(config)
+
+  return config;
+};
+
+
+const adjustDevServer = () => config => {
+  // config.disableHostCheck = true
+  // config.headers = config.headers || {}
+  // config.headers['Access-Control-Allow-Origin'] = '*'
+  // return config
+  config.headers = {
+    'Access-Control-Allow-Origin': '*',
+  };
+  config.hot = false;
+  config.watchContentBase = false;
+  // config.liveReload = false;
+  return config;
+}
+
+
+
+
+module.exports = {
+  webpack: override(
+    fixBabelImports("antd", {
+      libraryName: "antd",
+      libraryDirectory: "es",
+      style: true
+    }),
+    addWebpackPlugin(
+      new CompressionPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8,
+        cache: true
+      }),
+    ),
+
+    addLessLoader({
+      javascriptEnabled: true,
+      // modifyVars: { "@primary-color": "red" }
+    }),
+    addWebpackAlias({
+      "@": resolve("src"),
+      "src": resolve("src")
+    }),
+    adjustOutput()
+  ),
+  devServer: overrideDevServer(
+    // dev server plugin
+    adjustDevServer()
+  )
+};
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 
+import './public-path';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -6,13 +7,37 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store'
 
-ReactDOM.render(
-  // 2、然后使用react-redux的Provider将props与容器连通起来
-  <Provider store={ store }>
+
+function render(props) {
+  const { container } = props;
+  ReactDOM.render(
+    // 2、然后使用react-redux的Provider将props与容器连通起来
+    <Provider store={store}>
       <App />
-  </Provider> ,
-  document.getElementById('root') as HTMLElement
-);
+    </Provider>,
+    container ? container.querySelector('#root') : document.querySelector('#root')
+  );
+
+  ReactDOM.render(<App />, container ? container.querySelector('#root') : document.querySelector('#root'));
+}
+
+if (!(window as any).__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log('[react16] react app bootstraped');
+}
+export async function mount(props) {
+  console.log('[react16] props from main framework', props);
+  render(props);
+}
+
+export async function unmount(props) {
+  const { container } = props;
+  ReactDOM.unmountComponentAtNode(container ? container.querySelector('#root') : document.querySelector('#root'));
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
