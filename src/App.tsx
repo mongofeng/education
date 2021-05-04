24ebd34e21463fdc575f80a583463ac93e3ce75c
yangpu <1325896386@qq.com>
2021-05-04 15:52:53 +0800

    feat: 修改common

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { ConfigProvider } from 'antd';
 import zh_CN from 'antd/lib/locale-provider/zh_CN';
 import 'moment/locale/zh-cn';
 import * as React from 'react';
-import './asset/scss/main.scss';
 import { renderRoutes } from 'react-router-config';
+import './asset/scss/main.scss';
 import routes from './router';
 import store from './store'
 
-import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
+import { HashRouter } from "react-router-dom";
 const moment = require('moment');
 moment.locale('zh-cn');
 
@@ -20,7 +20,7 @@ class App extends React.PureComponent {
       // 2、然后使用react-redux的Provider将props与容器连通起来
     <Provider store={store}>
       <ConfigProvider locale={zh_CN}>
-        <HashRouter basename="yangjin-min">
+        <HashRouter basename="app-common">
           <div className="primary-layout">
             {renderRoutes(routes)}
           </div>
