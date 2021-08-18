e49dc1816f90e8a412b88b84a0ad3a7cec821952
yangpu <mongofeng@163.com>
2021-08-18 13:49:33 +0800

    feat: 添加概览

--- src/page/hour/TimeCount.tsx
+++ src/page/hour/TimeCount.tsx
@@ -1,17 +1,36 @@
+import { getHourrList } from '@/api/hour'
 import { commonQuery } from '@/api/statistics'
 import fetchTeacherHook from '@/common/hooks/teacher'
-import { COURSE_HOUR_ACTION_TYPE } from '@/const/enum'
+import { COURSE_HOUR_ACTION_TYPE, WEEK_LABEL } from '@/const/enum'
+import * as enums from '../../const/enum'
 import { IHour } from '@/const/type/hour'
-import { Drawer, Table } from 'antd'
+import formatDate from '@/utils/format-date'
+import { Button, Drawer, Radio, Table, Tag } from 'antd'
 import { ColumnProps } from 'antd/lib/table'
 import dayjs from 'dayjs'
 import * as React from 'react'
+import { connect } from 'react-redux'
+import { ICourse } from '@/const/type/student-operation'
 
+interface IProps {
+  package: {
+    [key in string]: string;
+  };
+  student: {
+    [key in string]: string;
+  }
+  openIds: Map<string, string[]>
+}
 
-export default function TimeCount (): JSX.Element {
+function TimeCount(props: IProps): JSX.Element {
   const [visible, setVisible] = React.useState(false)
   const [data, setData] = React.useState([]);
+  const [hd, setHD] = React.useState([]);
+  const [offset, setOffset] = React.useState(7);
   const [loading, setLoading] = React.useState(false);
+  const offsetList = React.useMemo(() => [7, 30, 60], [])
+
+
 
   const {
     teacherObj,
@@ -20,30 +39,134 @@ export default function TimeCount (): JSX.Element {
   })
 
 
+  const cols: Array<ColumnProps<IHour>> =  [
+      {
+        title: "学员",
+        dataIndex: "studentId",
+        render: (val: string) => {
+          return props.student[val]
+        }
+      },
+
+      {
+        title: "所属老师",
+        dataIndex: "teacherId",
+        render: (teacherId: string, row) => {
+
+          return teacherObj[teacherId]
+        }
+      },
+
+      {
+        title: "课时数量",
+        dataIndex: "num",
+      },
+      {
+        title: "操作类型",
+        dataIndex: "type",
+        render: (str: enums.COURSE_HOUR_ACTION_TYPE) => {
+          return (
+            <Tag color={enums.COURSE_HOUR_ACTION_TYPE_COLOR[str]}>
+              {enums.COURSE_HOUR_ACTION_TYPE_LABEL[str]}
+            </Tag>
+          )
+        }
+      },
+      {
+        title: "课程",
+        dataIndex: "course",
+        render: (val: ICourse[]) => {
+          if (!val || !val.length) {
+            return '-'
+          }
+          return val.map((item) => {
+            return [
+              (
+                <div key={item.id}>
+                  {`${item.name}:${item.count}课时`}
+                </div>
+              )
+            ]
+          })
+        }
+      },
+      {
+        title: "创建时间",
+        dataIndex: "createDate",
+        render: (date: string) => formatDate(new Date(date))
+      },]
+
+
+
   const columns: Array<ColumnProps<IHour>> = [
-    {
-      title: "学员",
-      dataIndex: "date",
-    },
+      {
+        title: "时间",
+        dataIndex: "date",
+        render: (value, row: any) => {
+          const obj = {
+            children: value,
+            props: {
+              rowSpan: row.rowSpan
+            },
+          };
+          return obj;
+        },
+      },
 
-    {
-      title: "所属老师",
-      dataIndex: "teacherId",
-      render: (teacherId: string, row) => {
-  
-        return teacherObj[teacherId]
-      }
-    },
+      {
+        title: "周",
+        dataIndex: "week",
+        render: (v) => WEEK_LABEL[v - 1]
+      },
 
-    {
-      title: "课时数量",
-      dataIndex: "num",
-    },]
+      {
+        title: "所属老师",
+        dataIndex: "teacherId",
+        render: (teacherId: string, row) => {
+
+          return teacherObj[teacherId]
+        }
+      },
+
+      {
+        title: "课时数量",
+        dataIndex: "num",
+      },
+
+
+
+
+      {
+        title: "学生",
+        dataIndex: "count",
+        render: (value, row) => {
+          return (
+            <Button type="link" onClick={() => onShowDetail(row)}>{value}</Button>
+          )
+        }
+      },
+
+      {
+        title: "合计",
+        dataIndex: "total",
+        render: (value, row: any) => {
+          const obj = {
+            children: value,
+            props: {
+              rowSpan: row.rowSpan
+            },
+          };
+          return obj;
+        },
+      },
+
+
+    ]
+
 
 
   const params = React.useMemo(() => {
-    const currentDate  = new Date()
-    const  offset = 7
+    const currentDate = new Date()
     currentDate.setDate(currentDate.getDate() - offset)
     const today = dayjs(currentDate).format('YYYY-MM-DD 00:00:00 +0800')
     const teacherMatch = JSON.stringify({
@@ -55,12 +178,17 @@ export default function TimeCount (): JSX.Element {
       }
     })
 
-    // 通用的group
+    // 通用的group，Returns the day of the week for a date as a number between 1 (Sunday) and 7 (Saturday).
     const group = JSON.stringify(
       {
         $group: {
-           _id: { date: {$dateToString: { format: '%Y-%m-%d', date: '$createDate' }}, teacherId: '$teacherId' },
-          count: { $sum: '$num' },
+          _id: { date: { $dateToString: { format: '%Y-%m-%d', date: '$createDate' } }, teacherId: '$teacherId' },
+          num: { $sum: '$num' },
+          id: {
+            $first: {
+              $toString: '$_id'
+            }
+          },
           ids: {
             $push: {
               $toString: '$_id'
@@ -71,18 +199,19 @@ export default function TimeCount (): JSX.Element {
               $dayOfWeek: '$createDate'
             }
           },
+
           createDate: {
             $first: '$createDate'
           },
-          total: {$sum: 1}
+          count: { $sum: 1 }
         }
       }
     )
 
 
 
-    // 通用的group
-    const group2 = JSON.stringify(
+
+    /* const group2 = JSON.stringify(
       {
         $group: {
            _id: '$_id.date',
@@ -100,7 +229,7 @@ export default function TimeCount (): JSX.Element {
           }
         }
       }
-    )
+    ) */
 
 
     const sort = JSON.stringify({ $sort: { createDate: -1 } })
@@ -110,19 +239,65 @@ export default function TimeCount (): JSX.Element {
       sql: [
         teacherMatch,
         group,
-    
+
         sort
       ]
     }
+  }, [offset])
+
+
+  const onChange = React.useCallback((e) => {
+    setOffset(e.target.value)
+  }, [])
+
+
+  const onShowDetail = React.useCallback((e) => {
+    setVisible(true)
+    fetchHourrList(e.ids)
   }, [])
 
 
   const fetchData = async () => {
 
     try {
+      setLoading(true);
       const { data: { data } } = await commonQuery(params)
-      console.warn(data)
+
+      const map = new Map()
+      let v
+      data.forEach(i => {
+        v = map.get(i.date)
+        if (v) {
+          v.rowSpan += 1
+          v.total += i.num
+
+          i.rowSpan = 0
+        } else {
+          i.rowSpan = 1
+          i.total = i.num
+          map.set(i.date, i)
+        }
+      })
+
       setData(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+
+  const fetchHourrList = async (ids) => {
+    const params: any = {
+      "size": ids.length, "limit": ids.length, "page": 1, "query": {
+        _id: {
+          $in: ids
+        }
+      }, "sort": { "createDate": -1 }
+    }
+    try {
+      const { data: { data: { list } } } = await getHourrList(params)
+      setHD(list);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -133,7 +308,7 @@ export default function TimeCount (): JSX.Element {
     fetchData();
   }, [params]);
 
-  
+
   return (
     <div>
       <div className="main-title clearfix">
@@ -144,25 +319,46 @@ export default function TimeCount (): JSX.Element {
       <Drawer
         title="详情"
         placement="right"
-        closable={false}
+        width={800}
+        closable={true}
         onClose={() => setVisible(false)}
-        visible={visible}
-      >
-    
+        visible={visible}>
+
+        <Table
+          bordered={true}
+          columns={cols}
+          dataSource={hd}
+          rowKey="id" ></Table>
+
       </Drawer>
 
       <div className="content-wrap">
 
-
+        <div style={{ marginBottom: 10 }}>
+          <Radio.Group defaultValue={offset} onChange={onChange}>
+            {React.useMemo(() => offsetList.map(i => {
+              return <Radio.Button value={i} key={i}>{i + '天'}</Radio.Button>
+            }), [offsetList])}
+          </Radio.Group>
+        </div>
 
 
         <Table
           bordered={true}
           columns={columns}
           dataSource={data}
-          rowKey="_id"
+          rowKey="id"
           loading={loading} ></Table>
       </div>
     </div>
   )
 }
+
+
+export default connect((state: any) => {
+  return {
+    student: state.student.labels,
+    openIds: state.student.openIds,
+    package: state.package.labels,
+  }
+})(TimeCount);
